dc450521d07e646f5537db42e9ee71a86e5ad536
Arturoo09 <codealgr@gmail.com>
2022-08-02 11:43:56 +0200

    avance en Contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,24 +1,41 @@
-import React, {useRef} from 'react'
+import React, {useContext, useRef, useState} from 'react'
 import emailjs from '@emailjs/browser'
 import './Contact.css'
+import { themeContext } from "../../Context";
 
 const Contact = () => {
+    const theme = useContext(themeContext);
+    const darkMode = theme.state.darkMode;
     const form = useRef();
+    const [done, setDone] = useState(false)
 
     const sendEmail = (e) => {
         e.preventDefault();
 
         emailjs.sendForm('service_zwih21l', 'template_z81kjtj', form.current, 'TVKP97vAriwjufPeD').then((result) => {
             console.log(result.text);
+            setDone(true);
+            form.reset();
         }, (error) => {
             console.log(error.text);
         });
 
     };
     return (
-    <form ref={form} onSubmit={sendEmail}>
-      
-    </form>
+        <div className="contact" id="contact">
+            <div className="c-left">
+                <span style={{color: darkMode ? 'white': ''}}>Contact me</span>
+            </div>
+            <div className="c-right">
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name='user_name' className='user' placeholder='Name'/>
+                    <input type="email" name='user_email' className='user' placeholder='Email'/>
+                    <textarea name="message" className="user" placeholder="Message"/>
+                    <input type="submit" value="Send" className="button"/>
+                    <span>{done && "Thanks for Contacting me"}</span>
+                </form>
+            </div>
+        </div>
     )
 }
 
