eed975b4c2334270572f178c020a88e71eff2dcc
Arturoo09 <codealgr@gmail.com>
2022-08-03 23:05:54 +0200

    Edit Arturo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Navbar.css'
 import Toggle from '../Toggle/Toggle' 
+import { themeContext } from "../../Context";
+import { useContext } from "react";
 import { Link } from 'react-scroll'
 const Navbar = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
     <div className="n-wrapper">
         <div className="n-left">
-            <div className="n-name">Arturo</div>
+            <div className="n-name">
+                <span style={{color: darkMode ? 'white' : ''}}>A</span>
+                <span style={{color: darkMode ? 'white' : ''}}>R</span>
+                <span style={{color: darkMode ? 'white' : ''}}>T</span>
+                <span style={{color: darkMode ? 'white' : ''}}>U</span>
+                <span style={{color: darkMode ? 'white' : ''}}>R</span>
+                <span style={{color: darkMode ? 'white' : ''}}>O</span>
+            </div>
             <Toggle />
         </div>
         <div className="n-right">
